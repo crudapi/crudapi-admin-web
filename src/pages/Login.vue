b4a383548e24ef954cca55ed69f76b912f7874b6
crudapi <admin@crudapi.cn>
2021-09-27 18:17:57 +0800

    enhance login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,61 +1,66 @@
 <template>
   <div class="col body">
-    <div class="row text-center justify-start text-white logo-icon">
-      <div class="logo"><img alt="logo" src="crudapi-mini.png" /></div>
-    </div>
-    <div class="row justify-center login">
-      <div class="left">
-        <div class="title">crudapi管理员后台</div>
-        <div class="sub-title"></div>
-        <div class="logo"><img src="crudapi.png" /></div>
+    <div class="q-pa-md row text-center justify-start text-white logo-icon">
+      <div class="logo col-2" >
+        <div  style="height: 100%;" class="row justify-center items-center content-center">
+           <img class="" alt="logo" src="crudapi-mini.png" />
+        </div>
       </div>
-      <div class="right">
-        <div class="title text-h6 text-center">
-          <q-item-label>登录</q-item-label>
-        </div>
-        <div class="mobile row justify-center">
-          <q-input
-            placeholder="请输入用户名"
-            v-model="username"
-            style="width: 336px;"
-          >
-            <template v-slot:prepend>
-              <q-item-label>用户名</q-item-label>
-            </template>
-          </q-input>
-        </div>
-        <div class="yzm row justify-center">
-          <q-input
-            placeholder="请输入密码"
-            v-model="password"
-            style="width: 336px;"
-            :type="isPwd ? 'password' : 'text'"
-            v-on:keyup.enter="submit"
-          >
-            <template v-slot:prepend>
-              <q-item-label>密码</q-item-label>
-            </template>
-            <template v-slot:append>
-              <q-icon
-                :name="isPwd ? 'visibility_off' : 'visibility'"
-                class="cursor-pointer"
-                @click="isPwd = !isPwd"
-              />
-            </template>
-          </q-input>
-        </div>
-        <div class="row justify-center">
-          <q-btn
-            unelevated
-            class="login-btn"
-            color="primary"
-            label="登 录"
-            :disable="!username || !password"
-            @click="submit"
-          />
+      <div class="col-10">
+        <div  style="height: 100%;" class="row justify-center items-center content-center">
+          <div class="welcome">欢迎使用crudapi增删改查接口零代码开发平台</div>
         </div>
       </div>
     </div>
+
+    <div class="row justify-center">
+        <div class="login-form">
+          <div class="title text-h6 text-center">
+            <q-item-label>登录</q-item-label>
+          </div>
+          <div class="mobile row justify-center">
+            <q-input
+              placeholder="请输入用户名"
+              v-model="username"
+              style="width: 336px;"
+            >
+              <template v-slot:prepend>
+                <q-item-label>用户名</q-item-label>
+              </template>
+            </q-input>
+          </div>
+          <div class="yzm row justify-center">
+            <q-input
+              placeholder="请输入密码"
+              v-model="password"
+              style="width: 336px;"
+              :type="isPwd ? 'password' : 'text'"
+              v-on:keyup.enter="submit"
+            >
+              <template v-slot:prepend>
+                <q-item-label>密码</q-item-label>
+              </template>
+              <template v-slot:append>
+                <q-icon
+                  :name="isPwd ? 'visibility_off' : 'visibility'"
+                  class="cursor-pointer"
+                  @click="isPwd = !isPwd"
+                />
+              </template>
+            </q-input>
+          </div>
+          <div class="row justify-center">
+            <q-btn
+              unelevated
+              class="login-btn"
+              color="primary"
+              label="登 录"
+              :disable="!username || !password"
+              @click="submit"
+            />
+          </div>
+        </div>
+    </div> 
 
     <div class="row text-center justify-center copyright">
       Copyright © 2020-present crudapi &nbsp;<a target="_blank" href="http://beian.miit.gov.cn/">苏ICP备15043861号-2</a> &nbsp; All Rights Reserved 版权所有
@@ -72,12 +77,9 @@
 
 .logo-icon
   height:145px;
-
+  .welcome
+    color: black;
   .logo
-    margin-top: 36px;
-    margin-left: 54px;
-    width:165px;
-    height:48px;
     img
       width:64px;
       height:64px;
@@ -90,84 +92,50 @@
   color:rgba(153,153,153,1);
   line-height:14px;
 
-.login
+.login-form
+  width: 100%;
   height: 490px;
-  min-width: 1030px;
-  .left
-    width: 400px;
-    height: 490px;
-    background: #3D3F4B;
-    border-radius:12px 0px 0px 12px;
-    .title
-      margin-top: 58px;
-      margin-left: 31px;
-      width:216px;
-      height:33px;
-      font-size:24px;
-      font-family:PingFangSC-Semibold,PingFang SC;
-      font-weight:600;
-      color:rgba(255,255,255,1);
-      line-height:33px;
-    .sub-title
-      margin-top: 8px;
-      margin-left: 31px;
-      width:285px;
-      height:34px;
-      font-size:15px;
-      font-family:PingFangSC-Regular,PingFang SC;
-      font-weight:400;
-      color:rgba(255,255,255,1);
-      line-height:34px;
-    .logo
-      img
-        margin-left: 31px;
-        width:320px;
-        height:320px;
-
-  .right
-    width: 560px;
-    height: 490px;
-    background:rgba(255,255,255,1);
-    box-shadow:0px 4px 24px 0px rgba(172,178,192,0.15);
-    border-radius:0px 12px 12px 0px;
-    .title
-      margin-top: 74px;
-      font-size:22px;
-      font-family:PingFangSC-Semibold,PingFang SC;
-      font-weight:600;
+  background:rgba(255,255,255,1);
+  box-shadow:0px 4px 24px 0px rgba(172,178,192,0.15);
+  border-radius:0px 12px 12px 0px;
+  .title
+    margin-top: 74px;
+    font-size:22px;
+    font-family:PingFangSC-Semibold,PingFang SC;
+    font-weight:600;
+    color:rgba(51,51,51,1);
+    line-height:30px;
+  .mobile
+    margin-top: 74px;
+    .q-item__label
+      height:20px;
+      font-size:14px;
+      font-family:PingFangSC-Medium,PingFang SC;
+      font-weight:500;
       color:rgba(51,51,51,1);
-      line-height:30px;
-    .mobile
-      margin-top: 74px;
-      .q-item__label
-        height:20px;
-        font-size:14px;
-        font-family:PingFangSC-Medium,PingFang SC;
-        font-weight:500;
-        color:rgba(51,51,51,1);
-        line-height:20px;
-    .yzm
-      margin-top: 34px;
-      .q-item__label
-        height:20px;
-        font-size:14px;
-        font-family:PingFangSC-Medium,PingFang SC;
-        font-weight:500;
-        color:rgba(51,51,51,1);
-        line-height:20px;
-      .yzm-action
-        height:20px;
-        font-size:14px;
-        font-family:PingFangSC-Medium,PingFang SC;
-        font-weight:500;
-        color:rgba(52,200,231,1);
-        line-height:20px;
-    .login-btn
-      margin-top:38px;
-      width:336px;
-      height:54px;
-      background:rgba(52,200,231,1);
-      border-radius:27px;
+      line-height:20px;
+  .yzm
+    margin-top: 34px;
+    .q-item__label
+      height:20px;
+      font-size:14px;
+      font-family:PingFangSC-Medium,PingFang SC;
+      font-weight:500;
+      color:rgba(51,51,51,1);
+      line-height:20px;
+    .yzm-action
+      height:20px;
+      font-size:14px;
+      font-family:PingFangSC-Medium,PingFang SC;
+      font-weight:500;
+      color:rgba(52,200,231,1);
+      line-height:20px;
+  .login-btn
+    margin-top:38px;
+    width:336px;
+    height:54px;
+    background:rgba(52,200,231,1);
+    border-radius:27px;
 </style>
 
 <script>
