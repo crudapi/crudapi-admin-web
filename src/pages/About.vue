d6ee14ee6e4f3f1f6ead2b063c4f747de933de44
crudapi <admin@crudapi.cn>
2022-11-22 18:04:45 +0800

    remove profile

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,31 +1,24 @@
 <template>
   <div class="q-pa-md bg-page">
     <div class="text-h6">简介</div>
-    <div class="q-pa-md">crudapi是crud+api组合，表示增删改查接口，是一款零代码可配置的产品。使用crudapi可以告别枯燥无味的增删改查代码，让您更加专注业务，节约大量成本，从而提高工作效率。crudapi的目标是让处理数据变得更简单，所有人都可以免费使用！无需编程，通过配置自动生成crud增删改查RESTful API，提供后台Web管理UI。基于主流的开源框架，前端采用Vue + Quasar，后端采用Spring Boot + Jdbc， 数据库支持MySQL、PostgreSQL、SQL Server和Oracle，拥有自主知识产权，支持二次开发，包括API和SDK两种方式。永久免费（包括个人和商业使用），无功能限制。源码和定制开发请联系客服！</div>
+    <div class="q-pa-md">crudapi是crud+api组合，表示增删改查接口，是一款零代码可配置的产品。使用crudapi可以告别枯燥无味的增删改查代码，让您更加专注业务，节约大量成本，从而提高工作效率。crudapi的目标是让处理数据变得更简单，所有人都可以免费使用！无需编程，通过配置自动生成crud增删改查RESTful API，提供后台Web管理UI。基于主流的开源框架，前端采用Vue + Quasar，后端采用Spring Boot + Jdbc， 数据库支持MySQL、PostgreSQL、SQL Server和Oracle，支持二次开发，包括API和SDK两种方式。</div>
 
     <div class="text-h6">Web管理UI</div>
     <div class="q-pa-md">
-      基于Material Design风格的UI，界面美观，使用方面。支持元数据的管理和业务数据的增删改查操作！前端Vue代码crudapi-admin-web免费开源！<a target="_blank" href="https://help.crudapi.cn/crudapi-admin-web/helloworld.html">前端Vue环境搭建</a>
+      基于Material Design风格的UI，界面美观，使用方面。支持元数据的管理和业务数据的增删改查操作！前端Vue代码crudapi-admin-web开源！<a target="_blank" href="https://help.crudapi.cn/crudapi-admin-web/helloworld.html">前端Vue环境搭建</a>
     </div>
 
     <div class="text-h6">后台Java服务</div>
     <div class="q-pa-md">基于Java+Spring boot默认生成标准的RESTful CRUD API，支持标准的POST，PATCH，GET，DELETE等操作，丰富的Swagger API文档. <a target="_blank" href="https://help.crudapi.cn/crudapi-example/helloworld.html">后台Java环境搭建</a>
     </div>
 
-
-    <div class="text-h6">价格</div>
-    <div class="q-pa-md">
-      <div>永久免费（包括个人和商业使用），无功能限制。源码和定制开发请联系客服！</div>
-      <img width="450" alt="logo" src="price.png" />
-    </div>
-
     <div class="q-pt-md text-h6">联系方式</div>
       <div class="q-pa-md">
         QQ群：632034576<br/>
-        QQ客服：1440737304
+        QQ：1440737304
       </div>
       <div class="q-pa-md">
-      添加微信客服（暗号crudapi），加入crudapi开发者群可享源码版5折限时优惠！
+      添加微信（暗号crudapi），加入crudapi开发者群！
       <div align="left">
         <img width = "200" src="crudapiweixin.jpeg">
       </div>
